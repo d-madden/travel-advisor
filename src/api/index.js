4b2ba89028fac16c5fe69a0c07581bebd5b7772a
Dan <57277053+mddmn@users.noreply.github.com>
2022-06-08 00:56:34 -0400

    init commit

---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const options = {
+    params: {
+      bl_latitude: '11.847676',
+      tr_latitude: '12.838442',
+      bl_longitude: '109.095887',
+      tr_longitude: '109.149359',
+    },
+    headers: {
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY
+    }
+  };
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY
+          }
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
